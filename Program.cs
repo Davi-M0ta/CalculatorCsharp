f8062d6d3fead5924da33e97a4bc3f538a2a41b5
Davi da Mota <66802605+Davi-M0ta@users.noreply.github.com>
2023-01-23 12:19:47 -0300

    Adição de novas operações

--- Program.cs
+++ Program.cs
@@ -15,8 +15,10 @@ namespace Calculator
             Console.WriteLine("CALCULADORA");
             Console.WriteLine("Selecione o tipo de operação que deseja realizar: ");
             Console.WriteLine("");
-            Console.WriteLine("1 - Adição   2 - Subtração   3 - Multiplicação    4 - Divisão");
-            Console.WriteLine("5 - Sair");
+            Console.WriteLine("1 - Adição   2 - Subtração   3 - Multiplicação    4 - Divisão ");
+            Console.WriteLine("5 - Raiz Quadrada   6 - Fatorial   7 - Potenciação");
+            Console.WriteLine("");
+            Console.WriteLine("0 - Sair");
 
             Console.WriteLine("");
             Console.Write("Operação: ");
@@ -28,7 +30,10 @@ namespace Calculator
                 case 2: Subtraction(); break;
                 case 3: Multiplication(); break;
                 case 4: Division(); break;
-                case 5: System.Environment.Exit(0); break;
+                case 5: SquareRoot(); break;
+                case 6: Factorial(); break;
+                case 7: Potentiation(); break;
+                case 0: System.Environment.Exit(0); break;
                 default: Menu(); break;
             }
         }
@@ -37,10 +42,11 @@ namespace Calculator
         {
             Console.Clear();
 
-            Console.WriteLine("Primeiro Valor: ");
+            Console.Write("Primeiro Valor: ");
             float valor1 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
 
-            Console.WriteLine("Segundo Valor: ");
+            Console.Write("Segundo Valor: ");
             float valor2 = float.Parse(Console.ReadLine()!);
 
             Console.WriteLine("");
@@ -57,10 +63,11 @@ namespace Calculator
         {
             Console.Clear();
 
-            Console.WriteLine("Primeiro Valor: ");
+            Console.Write("Primeiro Valor: ");
             float valor1 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
 
-            Console.WriteLine("Segundo Valor: ");
+            Console.Write("Segundo Valor: ");
             float valor2 = float.Parse(Console.ReadLine()!);
 
             Console.WriteLine("");
@@ -77,10 +84,11 @@ namespace Calculator
         {
             Console.Clear();
 
-            Console.WriteLine("Primeiro Valor: ");
+            Console.Write("Primeiro Valor: ");
             float valor1 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
 
-            Console.WriteLine("Segundo Valor: ");
+            Console.Write("Segundo Valor: ");
             float valor2 = float.Parse(Console.ReadLine()!);
 
             Console.WriteLine("");
@@ -97,10 +105,11 @@ namespace Calculator
         {
             Console.Clear();
 
-            Console.WriteLine("Primeiro Valor: ");
+            Console.Write("Primeiro Valor: ");
             float valor1 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
 
-            Console.WriteLine("Segundo Valor: ");
+            Console.Write("Segundo Valor: ");
             float valor2 = float.Parse(Console.ReadLine()!);
 
             Console.WriteLine("");
@@ -109,6 +118,68 @@ namespace Calculator
 
             float resultado = (valor1 / valor2);
             Console.WriteLine($"O resultado da Divisão de {valor1} por {valor2} é: {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void SquareRoot()
+        {
+            Console.Clear();
+
+            Console.Write("Valor: ");
+            double valor1 = float.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("");
+            Console.WriteLine("====================================");
+            Console.WriteLine("");
+
+            double resultado = (Math.Sqrt(valor1));
+            Console.WriteLine($"O resultado da Raiz Quadrada de {valor1} é: {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Factorial()
+        {
+            Console.Clear();
+
+            Console.Write("Valor: ");
+            int valor = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("");
+            Console.WriteLine("====================================");
+            Console.WriteLine("");
+
+            int fatorial;
+
+            for (fatorial = 1; valor > 1; valor--)
+            {
+                fatorial = fatorial * valor;
+            }
+
+            Console.WriteLine($"O resultado do fatorial é: {fatorial}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Potentiation()
+        {
+            Console.Clear();
+
+            Console.Write("Insira o valor da base: ");
+            int valor = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("");
+
+            Console.Write("A qual valor deseja elevar ele? ");
+            int potencia = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("");
+            Console.WriteLine("====================================");
+            Console.WriteLine("");
+
+            double resultado = Math.Pow(valor, potencia);
+
+            Console.WriteLine($"O resultado da potenciação é: {resultado}");
             Console.ReadKey();
             Menu();
         }
